27aa42a5f308cbdab10a1c543a280f57ea79f1c3
Tsyren Tsynduev <tsyrentsynduev@gmail.com>
2020-05-13 12:33:20 +0300

    Добавлен try {} catch (){} для инициализации веб-элемента

--- src/test/java/page/Page.java
+++ src/test/java/page/Page.java
@@ -2,8 +2,12 @@ package page;
 
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.InvalidSelectorException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.NoSuchElementException;
 
 
 public class Page {
@@ -15,28 +19,34 @@ public class Page {
 
     }
 
-    @Step("Клик по элементу xpath")
-    public void click(WebDriver driver, String locator) {
+    @Step("Инициализация веб-элемента")
+    public WebElement getWebelement(WebDriver driver, String locator) {
+        WebElement element = null;
         try {
-            driver.findElement(By.xpath(locator)).click();
-        } catch (Exception a) {
+            element = driver.findElement(By.xpath(locator));
+        } catch (NoSuchElementException a) {
             System.out.println("Веб-элемент не найден");
             driver.quit();
+        } catch (InvalidSelectorException b) {
+            System.out.println("Невозможно найти элемент с выражением xpath");
+            driver.quit();
         }
+        return element;
+    }
+
+    @Step("Клик по элементу xpath")
+    public void click(WebDriver driver, String locator) {
+        getWebelement(driver, locator).click();
+
     }
 
     @Step("Ввод текста по локатору xpath")
     public void sendkeys(WebDriver driver, String locator, String text) {
-        try {
-            driver.findElement(By.xpath(locator)).sendKeys(text);
-        } catch (Exception a) {
-            System.out.println("Веб-элемент не найден");
-            driver.quit();
-        }
+        getWebelement(driver, locator).sendKeys(text);
     }
 
     @Step("Получение текста из области по xpath")
     public String gettext(WebDriver driver, String locator) {
-        return driver.findElement(By.xpath(locator)).getText();
+        return getWebelement(driver, locator).getText();
     }
 }
